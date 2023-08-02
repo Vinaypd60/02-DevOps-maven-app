934ffaa8e04088376226382b20717d0c81cf1231
vinay sai <vinaypd60@gmail.com>
2023-08-02 19:31:39 +0530

    my first commit

---
+++ src/main/java/01-Maven-App/App.java
@@ -0,0 +1,13 @@
+package 01-Maven-App;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
